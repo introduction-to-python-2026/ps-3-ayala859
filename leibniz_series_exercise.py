a0868fa2cf988f72195d815e40aeec86c17b1cdd
ayala859 <ayalla.goren@gmail.com>
2025-11-12 07:24:20 +0200

    Update leibniz_series_exercise.py

--- leibniz_series_exercise.py
+++ leibniz_series_exercise.py
@@ -1,8 +1,7 @@
 def approximate_pi(n_terms):
   pi_approximation = 0
   for i in range(n_terms):
-    num = 2 * i + 1
-    sign = (-1) ** i 
-    pi_approximation += sign * (1 / num)
+    num = ((-1)**i)/(2*i+1)
+    pi_approximation += num
   pi = 4 * pi_approximation
   return pi
